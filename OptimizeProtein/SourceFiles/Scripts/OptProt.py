bcbc11aa157898ec412e10cfab994142c8da094b
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-04-13 17:45:25 +0200

    Encapsulate implementation of perform_selected_computations() in several more methods. Change some global variables to instance variables to indicate that they could differ between different instances (which may run concurrently).

--- OptimizeProtein/SourceFiles/Scripts/OptProt.py
+++ OptimizeProtein/SourceFiles/Scripts/OptProt.py
@@ -2,30 +2,34 @@ import os
 import glob
 import subprocess
 import time
-import OptimizeProtein.yasara
+from OptimizeProtein import yasara
 
-OptimizeProtein.yasara.info.mode = 'txt'
+yasara.info.mode = 'txt'
 
 
 class OptProt:
     # declare all global variables
-    __pdb_list__ = []
-    __command__ = ''
-    __charge__ = ''
-    __molecules__ = ''
     __r_path__ = ''
     __foldx_path__ = ''
     __agadir_path__ = ''
     __qsub_path__ = ''
     __start_path__ = ''
     __scripts_path__ = ''
+    __execute_python_and_path_to_script__ = ''
+    __molecules_and_paths_to_r_foldx_agadir__ = ''
+    __molecules_and_paths_to_r_foldx_agadir_and_charge__ = ''
 
-    # initialiser
     def __init__(self, start_path, scripts_path):
         global __start_path__
         global __scripts_path__
+        global __execute_python_and_path_to_script__
         __start_path__ = start_path
         __scripts_path__ = scripts_path
+        __execute_python_and_path_to_script__ = 'python ' + __scripts_path__ + '/'
+        self.__pdb_list__ = []
+        self.__command__ = ''
+        self.__charge__ = ''
+        self.__molecules__ = ''
 
     # Extracts instructions from the option file.
     # This includes which computations to run & which proteins to run them on in 3 parts:
@@ -36,72 +40,57 @@ class OptProt:
         self.__build_pdb_list_from(__option_file__)
         self.__parse_cmd_charge_mols_from(__option_file__)
         self.__parse_paths_for_r_foldx_agadir_qsub_from(__option_file__)
+        global __molecules_and_paths_to_r_foldx_agadir__
+        global __molecules_and_paths_to_r_foldx_agadir_and_charge__
+        __molecules_and_paths_to_r_foldx_agadir__ = ' ' + self.__molecules__ + ' ' + __r_path__ + ' ' + __foldx_path__ + \
+                                                    ' ' + __agadir_path__
+        __molecules_and_paths_to_r_foldx_agadir_and_charge__ = __molecules_and_paths_to_r_foldx_agadir__ + ' ' + \
+                                                               self.__charge__
 
     # Called by parse_option_file() for:
     # 1. list of pdb names
     def __build_pdb_list_from(self, __option_file__):
-        global __pdb_list__
-        __pdb_list__ = []
-
+        self.__pdb_list__ = []
         for line in __option_file__:
-
             if '#' in line:
                 continue
-
             if 'PDBs:' in line:
-
                 if line.split(':')[-1].strip(';\n') == "All":
                     pdbpaths = glob.glob('./PDBs/*.pdb')
-
                     for pdbpath in pdbpaths:
                         pdb_temp = pdbpath.split('/')[-1]
-                        __pdb_list__.append(pdb_temp)
+                        self.__pdb_list__.append(pdb_temp)
                 pdb_string = line.split(':')[-1].strip(';\n')
-
                 if ',' in pdb_string:
                     pdbs_temp = pdb_string.split(',')
-
                     for pdb_temp in pdbs_temp:
-                        __pdb_list__.append(pdb_temp)
-
+                        self.__pdb_list__.append(pdb_temp)
                 else:
-                    __pdb_list__.append(pdb_string)
-
-        print 'PDBs to analyse:\t\t' + ",\t".join(__pdb_list__)
+                    self.__pdb_list__.append(pdb_string)
+        print 'PDBs to analyse:\t\t' + ",\t".join(self.__pdb_list__)
 
     # Called by parse_option_file() for:
     # 2. computation names ("command"), charge, protein chains ("molecules")
     def __parse_cmd_charge_mols_from(self, __option_file__):
-        global __command__
-        global __charge__
-        global __molecules__
-
         for line in __option_file__:
-
             if '#' in line:
                 continue
-
             if 'Command:' in line:
-                __command__ = line.split(':')[-1].strip(';\n')
-
+                self.__command__ = line.split(':')[-1].strip(';\n')
             if 'Charge:' in line:
-                __charge__ = line.split(':')[-1].strip(';\n')
-
+                self.__charge__ = line.split(':')[-1].strip(';\n')
             if 'Mols:' in line:
-                __molecules__ = []
+                self.__molecules__ = []
                 MolString = line.split(':')[-1].strip(';\n')
-
                 if ',' in MolString:
                     Mols_temp = MolString.split(',')
-
                     for Mols_temp in Mols_temp:
-                        __molecules__.append(Mols_temp)
-                    __molecules__ = "_".join(__molecules__)
-
+                        self.__molecules__.append(Mols_temp)
+                    self.__molecules__ = "_".join(self.__molecules__)
                 else:
-                    __molecules__ = MolString
-        print 'Command to be executed:\t\t' + __command__
-        print 'Molecules to be considered:\t' + __molecules__
+                    self.__molecules__ = MolString
+        print 'Command to be executed:\t\t' + self.__command__
+        print 'Molecules to be considered:\t' + self.__molecules__
 
     # Called by parse_option_file() for:
     # 3. absolute paths to all required software and to the grid engine executables
@@ -110,18 +99,13 @@ class OptProt:
         global __foldx_path__
         global __agadir_path__
         global __qsub_path__
-
         for line in __option_file__:
-
             if 'R_Path:' in line:
                 __r_path__ = line.split(':')[-1].strip(';\n')
-
             if 'FoldX_Path:' in line:
                 __foldx_path__ = line.split(':')[-1].strip(';\n')
-
             if 'Agadir_Path:' in line:
                 __agadir_path__ = line.split(':')[-1].strip(';\n')
-
             if 'Qsub_Path' in line:
                 __qsub_path__ = line.split(':')[-1].strip(';\n')
         print 'Absolute path to R:\t\t' + __r_path__
@@ -136,17 +120,15 @@ class OptProt:
     def run_yasara_agadir_repair(self):
         if not os.path.exists('Results'):
             os.makedirs('Results')
-
-        for PDB in __pdb_list__:
+        for PDB in self.__pdb_list__:
             name = self._build_results_directory_tree_for_each(PDB)
+            # current directory is now inside Results folder
             yasara.run('DelObj all')
             yasara.run('LoadPDB ' + __start_path__ + '/PDBs/' + PDB)
             yasara.run('DelRes !Protein')
             tempMols = yasara.run('ListMol All,Format=MOLNAME')
-
             for mol in tempMols:
                 yasara.run('RenumberRes all and Mol ' + mol + ',First=1')
-
             yasara.run(
                 'SavePDB 1,' + __start_path__ + '/Results/' + name + '/PDBs/' + PDB + ',Format=PDB,Transform=Yes')
             subprocess.call('cp ' + __start_path__ + '/PDBs/' + PDB + ' ./PDBs/.', shell=True)
@@ -172,7 +154,6 @@ class OptProt:
     def wait_for_repair_to_complete(self):
         check_qstat = subprocess.Popen(__qsub_path__ + 'qstat', stdout=subprocess.PIPE)
         output_qstat = check_qstat.stdout.read()
-
         while 'RPjob_' in output_qstat:
             print 'Waiting for PDBs to be repaired'
             time.sleep(10)
@@ -180,92 +161,81 @@ class OptProt:
             output_qstat = check_qstat.stdout.read()
 
     def perform_selected_computations(self):
-
-        for PDB in __pdb_list__:
+        for PDB in self.__pdb_list__:
             self._build_results_directory_tree_for_each(PDB)
-            print 'stretchplot.py'
-            subprocess.call(
-                'python ' + __scripts_path__ + '/stretchplot.py ' + __molecules__ + ' ' + __r_path__ + ' ' + __foldx_path__ + ' ' + __agadir_path__,
-                shell=True)
-            print 'stretchplot.R'
-            subprocess.call('R < ' + __scripts_path__ + '/stretchplot.R --no-save', shell=True)
-
-            if not os.path.exists('Runs/' + __command__):
-
-                if __command__ != 'Stretchplot' and __command__ != 'All':
-                    os.makedirs('Runs/' + __command__)
-
-                if __command__ == 'All':
-                    os.makedirs('Runs/Stabilize')
-                    os.makedirs('Runs/Solubis')
-                    os.makedirs('Runs/SolubisMild')
-                    os.makedirs('Runs/CysScan')
-
-            if __command__ == 'Stabilize' or __command__ == 'All':
-                print 'stabilize.py'
-                subprocess.call(
-                    'python ' + __scripts_path__ + '/stabilize.py ' + __molecules__ + ' ' + __r_path__ + ' ' + __foldx_path__ + ' ' + __agadir_path__,
-                    shell=True)
-
-            if __command__ == 'Solubis' or __command__ == 'All':
-                print 'solubis.py'
-                subprocess.call(
-                    'python ' + __scripts_path__ + '/solubis.py ' + __molecules__ + ' ' + __r_path__ + ' ' + __foldx_path__ + ' ' + __agadir_path__,
-                    shell=True)
-
-            if __command__ == 'SolubisMild' or __command__ == 'All':
-                print 'solubis_mild.py'
-                subprocess.call(
-                    'python ' + __scripts_path__ + '/solubis_mild.py ' + __molecules__ + ' ' + __r_path__ + ' ' + __foldx_path__ + ' ' + __agadir_path__,
-                    shell=True)
-
-            if __command__ == 'CysScan' or __command__ == 'All':
-                print 'CysScan.py'
-                subprocess.call(
-                    'python ' + __scripts_path__ + '/CysScan.py ' + __molecules__ + ' ' + __r_path__ + ' ' + __foldx_path__ + ' ' + __agadir_path__,
-                    shell=True)
-
-            if __command__ == 'SolubisDouble':
-                print 'solubis_double.py'
-                subprocess.call(
-                    'python ' + __scripts_path__ + '/solubis_double.py ' + __molecules__ + ' ' + __r_path__ + ' ' + __foldx_path__ + ' ' + __agadir_path__,
-                    shell=True)
-
-            if __command__ == 'Supercharge':
-                print 'supercharge.py'
-                subprocess.call(
-                    'python ' + __scripts_path__ + '/supercharge.py ' + __molecules__ + ' ' + __r_path__ + ' ' + __foldx_path__ + ' ' + __agadir_path__ + ' ' + __charge__,
-                    shell=True)
-
-            if __command__ == 'DelPos':
-                print 'DelPos.py'
-                subprocess.call(
-                    'python ' + __scripts_path__ + '/DelPos.py ' + __molecules__ + ' ' + __r_path__ + ' ' + __foldx_path__ + ' ' + __agadir_path__ + ' ' + __charge__,
-                    shell=True)
-
-            if __command__ == 'Indiv':
-                print 'Indiv.py'
-                subprocess.call(
-                    'python ' + __scripts_path__ + '/Indiv.py ' + __molecules__ + ' ' + __r_path__ + ' ' + __foldx_path__ + ' ' + __agadir_path__ + ' ' + __charge__,
-                    shell=True)
-            subprocess.call('R < ' + __scripts_path__ + '/massplots.R --no-save', shell=True)
-            # subprocess.call('python '+__scripts_path__+'/clean.py',shell=True)
+            self._compute_stretchplot(PDB)
+            if not os.path.exists('Runs/' + self.__command__):
+                self._build_directory_tree_for_computations()
+            self._compute_commands()
             os.chdir(__start_path__)
 
     def _build_results_directory_tree_for_each(self, pdb):
         pdb_name = pdb.split('.')[0]
-
         if not os.path.exists('Results/' + pdb_name):
             os.makedirs('Results/' + pdb_name)
         os.chdir('Results/' + pdb_name)
-        self._create_folder_for('Repair')
-        self._create_folder_for('Agadir')
-        self._create_folder_for('Runs')
-        self._create_folder_for('PDBs')
-        self._create_folder_for('Fasta')
+        self._create_folder_for('Repair', 'Agadir', 'Runs', 'PDBs', 'Fasta')
         return pdb_name
 
-    def _create_folder_for(self, folder_name):
+    def _create_folder_for(self, *args):
+        for folder_name in args:
+            if not os.path.exists(folder_name):
+                os.makedirs(folder_name)
 
-        if not os.path.exists(folder_name):
-            os.makedirs(folder_name)
+    def _compute_stretchplot(self, PDB):
+        print 'OptProt.py calling stretchplot.py'
+        subprocess.call(
+            'python ' + __scripts_path__ + '/stretchplot.py ' + __molecules_and_paths_to_r_foldx_agadir__,
+            shell=True)
+        print 'OptProt.py calling stretchplot.R'
+        subprocess.call('R < ' + __scripts_path__ + '/stretchplot.R --no-save', shell=True)
+
+    def _build_directory_tree_for_computations(self):
+        if self.__command__ == 'All':
+            self._build_directory_tree_for_stabilize_solubis_solubismild_cysscan()
+        elif self.__command__ != 'Stretchplot':
+            self._create_Runs_subfolder_for(self.__command__)
+
+    def _build_directory_tree_for_stabilize_solubis_solubismild_cysscan(self):
+        self._create_Runs_subfolder_for('Stabilize', 'Solubis', 'SolubisMild', 'CysScan')
+
+    def _create_Runs_subfolder_for(self, *args):
+        for command in args:
+            os.makedirs('Runs/' + command)
+
+    def _compute_commands(self):
+        if self.__command__ == 'All':
+            self._compute_stabilize_solubis_solubismild_cysscan()
+        else:
+            self._compute(self.__command__)
+
+    def _compute_stabilize_solubis_solubismild_cysscan(self):
+        self._compute_('Stabilize', 'Solubis', 'SolubisMild', 'CysScan')
+
+    def _compute(self, *args):
+        for command in args:
+            python_script = self._convert_command_name_to_python_script_name(command)
+            print 'OptProt.py calling ' + python_script + '....'
+            if self.__command__ == 'Supercharge' or self.__command__ == 'DelPos' or self.__command__ == 'Indiv':
+                subprocess.call(
+                    __execute_python_and_path_to_script__ + python_script +
+                    __molecules_and_paths_to_r_foldx_agadir_and_charge__,
+                    shell=True)
+            else:
+                subprocess.call(
+                    __execute_python_and_path_to_script__ + python_script + __molecules_and_paths_to_r_foldx_agadir__,
+                    shell=True)
+
+    def _convert_command_name_to_python_script_name(self, command):
+        python_script_name = command
+        if self.__command__ == 'Stabilize':
+            python_script_name = 'stabilize'
+        elif self.__command__ == 'Solubis':
+            python_script_name = 'solubis'
+        elif self.__command__ == 'SolubisMild':
+            python_script_name = 'solubis_mild'
+        elif self.__command__ == 'SolubisDouble':
+            python_script_name = 'solubis_double'
+        elif self.__command__ == 'Supercharge':
+            python_script_name = 'supercharge'
+        return python_script_name + '.py'
