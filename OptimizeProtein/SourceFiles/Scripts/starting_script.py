ce0ec9c37c85d44b8163a65ec2e4cbd9c2ba1fa7
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-07-03 18:35:12 +0200

    Improve variable name.

--- OptimizeProtein/SourceFiles/Scripts/starting_script.py
+++ OptimizeProtein/SourceFiles/Scripts/starting_script.py
@@ -1,7 +1,7 @@
 from OptProt import OptProt
 import yaml
 
-opt_prot_instance = ''
+opt_prot = ''
 
 with open("/switchlab/group/shazib/OptimizeProteinShazibCopy/SourceFiles/Scripts/pathsAndDictionaries.yaml",
           'r') as stream:
@@ -14,11 +14,11 @@ with open("/switchlab/group/shazib/OptimizeProteinShazibCopy/SourceFiles/Scripts
         foldx_path = paths_and_dictionaries['ROOT']['FoldX_path']
         agadir_path = paths_and_dictionaries['ROOT']['Agadir_path']
         qsub_path = paths_and_dictionaries['ROOT']['Qsub_path']
-        opt_prot_instance = OptProt(start_path, scripts_path, r_path, foldx_path, agadir_path, qsub_path)
+        opt_prot = OptProt(start_path, scripts_path, r_path, foldx_path, agadir_path, qsub_path)
 
     except yaml.YAMLError as exc:
         print(exc)
 
-opt_prot_instance.parse_option_file(open('./OptProt_Options.txt', 'r').readlines())
-opt_prot_instance.run_yasara_agadir_repair()
-opt_prot_instance.perform_selected_computations()
+opt_prot.parse_option_file(open('./OptProt_Options.txt', 'r').readlines())
+opt_prot.run_yasara_agadir_repair()
+opt_prot.perform_selected_computations()
